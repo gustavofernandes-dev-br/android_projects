fad1483f2a0c652d6413f281796adba3b5f943ca
Gustavo <gustavo@pdvnet.com.br>
2021-08-29 21:45:53 -0300

    Layout novo de login e re fatorações de código

---
+++ organize/app/src/main/java/br/dev/gustavofernandes/organize/activity/PrincipalLoginActivity.java
@@ -0,0 +1,16 @@
+package br.dev.gustavofernandes.organize.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.dev.gustavofernandes.organize.R;
+
+public class PrincipalLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_login);
+    }
+}
