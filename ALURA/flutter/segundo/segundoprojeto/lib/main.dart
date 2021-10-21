b4cf03449727ffe204066534e44c3c9baef2513e
Gustavo <gustavo@pdvnet.com.br>
2021-10-21 01:05:15 -0300

    treinamento

--- ALURA/flutter/segundo/segundoprojeto/lib/main.dart
+++ ALURA/flutter/segundo/segundoprojeto/lib/main.dart
@@ -8,12 +8,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Column(
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Home> {
+
+  int _contador = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          "Pessoas : 0",
+          "Pessoas : $_contador",
           style: TextStyle(
               fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
         ),
@@ -23,7 +37,13 @@ class MyApp extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      _contador++;
+                    });
+
+
+                  },
                   child: Text(
                     "+1",
                     style: TextStyle(fontSize: 20.0, color: Colors.white),
@@ -32,7 +52,12 @@ class MyApp extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      _contador--;
+                    });
+
+                  },
                   child: Text(
                     "-1",
                     style: TextStyle(fontSize: 20.0, color: Colors.white),
@@ -46,6 +71,6 @@ class MyApp extends StatelessWidget {
               fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ],
-    ));
+    );
   }
 }
